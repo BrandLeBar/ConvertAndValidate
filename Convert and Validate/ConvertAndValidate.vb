84961eb370aa51d5ea798ac37dbe29155b9c1542
Barkly-Brandon <bubba05barrera@gmail.com>
2025-02-16 16:42:47 -0700

    Add project files.

---
+++ Convert and Validate/ConvertAndValidate.vb
@@ -0,0 +1,56 @@
+﻿'Brandon Barrera
+'Spring 2025
+'RCET2265
+'Convert and Validate
+'
+
+Option Compare Text
+Option Strict On
+Option Explicit On
+
+Module ConvertAndValidate
+
+    Sub Main()
+
+        Dim aValidNumber As Integer
+        Dim userResponse As String
+
+        Do
+
+            Console.WriteLine($"Enter a number:")
+
+            userResponse = Console.ReadLine()
+
+
+            If ConversionValid(userResponse, aValidNumber) = True Then
+
+                Console.WriteLine($"{userResponse} converted successfully to {aValidNumber}!")
+
+            Else
+
+                Console.WriteLine($"Oops, {userResponse} does not seem to be a number")
+
+            End If
+
+        Loop
+
+    End Sub
+
+    'this works now
+
+    Function ConversionValid(ByRef convertThisString As String, ByRef toThisInteger As Integer) As Boolean
+
+        Dim status As Boolean
+
+        Try
+            toThisInteger = CInt(convertThisString)
+            status = True
+        Catch ex As Exception
+            status = False
+        End Try
+
+        Return status
+
+    End Function
+
+End Module
---
+++ Convert and Validate/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Convert and Validate")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Convert and Validate")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e86f79be-af76-43f8-82e5-97c6c5a07f9f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
